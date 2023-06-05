c32fe3b33d3dd6787514c6fa3e3dc162f9753285
Butaforsky <59537160+Butaforsky@users.noreply.github.com>
2023-06-05 12:56:46 +0300

    add binary to decomal conversion

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -39,8 +39,20 @@
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
 #define HRTIM_FREQ 5440000000
-#define BASE HRTIM_FREQ / (100000 * 4)
-#define OVER HRTIM_FREQ / (105000 * 4)
+#define BASE       HRTIM_FREQ / (100000 * 4)
+#define OVER       HRTIM_FREQ / (105000 * 4)
+
+#define B(x) S_to_binary_(#x)
+
+static inline unsigned long long S_to_binary_(const char *s)
+{
+        unsigned long long i = 0;
+        while (*s) {
+                i <<= 1;
+                i += *s++ - '0';
+        }
+        return i;
+}
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -51,7 +63,7 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-uint16_t PERIOD = 0x308C; 
+uint16_t PERIOD                  = 0x308C;
 uint16_t adc_value               = 0;
 volatile uint16_t adc_value_prev = 0;
 static uint16_t dutyCycle        = 50;
@@ -71,12 +83,14 @@ struct ADC_t {
 
 struct ADC_t adc = {.value = 0, .low_threshold = 30, .high_threshold = 3900, .degrees_conv = 22};
 
-uint16_t flag    = 0;
+uint16_t flag     = 0;
 uint8_t freq_flag = 0;
 
 enum STATE state = STATE_START;
 
-char word_to_send[20] = {0,};
+char word_to_send[20] = {
+    0,
+};
 
 uint64_t hrtim_freq = 5440000000;
 /* USER CODE END PV */
@@ -93,47 +107,47 @@ int dec2bin(int num);
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
+    /* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+    /* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+    /* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+    HAL_Init();
 
-  /* USER CODE BEGIN Init */
+    /* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+    /* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+    /* Configure the system clock */
+    SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+    /* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+    /* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_DMA_Init();
-  MX_LPUART1_UART_Init();
-  MX_TIM1_Init();
-  MX_ADC1_Init();
-  MX_TIM2_Init();
-  MX_HRTIM1_Init();
-  MX_TIM3_Init();
-  /* USER CODE BEGIN 2 */
-    
+    /* Initialize all configured peripherals */
+    MX_GPIO_Init();
+    MX_DMA_Init();
+    MX_LPUART1_UART_Init();
+    MX_TIM1_Init();
+    MX_ADC1_Init();
+    MX_TIM2_Init();
+    MX_HRTIM1_Init();
+    MX_TIM3_Init();
+    /* USER CODE BEGIN 2 */
+
     HAL_ADC_Start(&hadc1);
     /* TIMA counter operating in continuous mode with prescaler = 010b (div.
  by 4) */
     /* Preload enabled on REP event*/
-    PERIOD = OVER;//BASE;//0x3B5E;//(uint16_t)(hrtim_freq / base_freq);
+    PERIOD = OVER; // BASE;//0x3B5E;//(uint16_t)(hrtim_freq / base_freq);
     // Real phase shift must be between PI/2 and PI
     // TODO: Saturate low phase shift at PI/2
     HRTIM1->sTimerxRegs[HRTIM_TIMERINDEX_TIMER_A].TIMxCR = HRTIM_TIMCR_CONT + HRTIM_TIMCR_PREEN + HRTIM_TIMCR_TREPU + HRTIM_TIMCR_CK_PSC_1;
@@ -156,31 +170,36 @@ int main(void)
                                HRTIM_OENR_TD1OEN + HRTIM_OENR_TD2OEN;
     /* Start Timer A and Timer D */
     HRTIM1->sMasterRegs.MCR = HRTIM_MCR_TACEN + HRTIM_MCR_TDCEN;
-    
-    HAL_UART_Receive_IT(&hlpuart1, (uint8_t*) &freq_flag, 1);
-    HAL_UART_Transmit(&hlpuart1, (uint8_t*) "Start\n", 8, 1000);
+
+    HAL_UART_Receive_IT(&hlpuart1, (uint8_t *)&freq_flag, 1);
+    HAL_UART_Transmit(&hlpuart1, (uint8_t *)"Start\n", 8, 1000);
 
     sprintf(word_to_send, "DECIMAL: %d\n", 10);
-    HAL_UART_Transmit(&hlpuart1, (uint8_t*) word_to_send, strlen(word_to_send), 1000);
-    
+    HAL_UART_Transmit(&hlpuart1, (uint8_t *)word_to_send, strlen(word_to_send), 1000);
+
     sprintf(word_to_send, "binary: %d\n", dec2bin(10));
-    HAL_UART_Transmit(&hlpuart1, (uint8_t*) word_to_send, strlen(word_to_send), 1000);
+    HAL_UART_Transmit(&hlpuart1, (uint8_t *)word_to_send, strlen(word_to_send), 1000);
+
+    int local_bin = 0;
+    local_bin = dec2bin(10);
+    sprintf(word_to_send, "binary [0]: %d\n", local_bin & (1 << 0));
+    HAL_UART_Transmit(&hlpuart1, (uint8_t *)word_to_send, strlen(word_to_send), 1000);
 
     // HAL_TIM_OC_Start(&htim1, TIM_CHANNEL_2);
     // HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
 
-  /* USER CODE END 2 */
+    /* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
     while (1) {
-    /* USER CODE END WHILE */
+        /* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
+        /* USER CODE BEGIN 3 */
         if (state == STATE_START) {
             /* set new tim CCRx registers based on ADC value */
-            HRTIM1->sTimerxRegs[HRTIM_TIMERINDEX_TIMER_A].CMP2xR = 
-            (PERIOD / 360 * (adc.value / adc.degrees_conv));
+            HRTIM1->sTimerxRegs[HRTIM_TIMERINDEX_TIMER_A].CMP2xR =
+                (PERIOD / 360 * (adc.value / adc.degrees_conv));
             HRTIM1->sTimerxRegs[HRTIM_TIMERINDEX_TIMER_A].CMP3xR =
                 (PERIOD / 2 + PERIOD / 360 * (adc.value / adc.degrees_conv));
             adc.value = HAL_ADC_GetValue(&hadc1);
@@ -190,14 +209,14 @@ int main(void)
                 adc.value = adc.low_threshold;
             }
             HAL_ADC_Start(&hadc1);
-        } 
+        }
 
         if (flag == 1) {
             /* push button pressed*/
             flag  = 0;
             state = !state; // toggle state
             if (state == STATE_STOP) {
-              /* set HRTIM CCRx to 0 */
+                /* set HRTIM CCRx to 0 */
                 HRTIM1->sTimerxRegs[HRTIM_TIMERINDEX_TIMER_A].CMP1xR = 0;
                 HRTIM1->sTimerxRegs[HRTIM_TIMERINDEX_TIMER_A].CMP2xR = 0;
                 HRTIM1->sTimerxRegs[HRTIM_TIMERINDEX_TIMER_A].CMP3xR = 0;
@@ -211,57 +230,53 @@ int main(void)
                 SET_BIT(GPIOA->ODR, GPIO_PIN_5);
                 HAL_ADC_Start(&hadc1);
                 HAL_TIM_Base_Start_IT(&htim1);
-
             }
         }
     }
-  /* USER CODE END 3 */
+    /* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage
-  */
-  HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1_BOOST);
+    /** Configure the main internal regulator output voltage
+     */
+    HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1_BOOST);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV4;
-  RCC_OscInitStruct.PLL.PLLN = 85;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
-  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    /** Initializes the RCC Oscillators according to the specified parameters
+     * in the RCC_OscInitTypeDef structure.
+     */
+    RCC_OscInitStruct.OscillatorType      = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState            = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.PLL.PLLState        = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource       = RCC_PLLSOURCE_HSI;
+    RCC_OscInitStruct.PLL.PLLM            = RCC_PLLM_DIV4;
+    RCC_OscInitStruct.PLL.PLLN            = 85;
+    RCC_OscInitStruct.PLL.PLLP            = RCC_PLLP_DIV2;
+    RCC_OscInitStruct.PLL.PLLQ            = RCC_PLLQ_DIV2;
+    RCC_OscInitStruct.PLL.PLLR            = RCC_PLLR_DIV2;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        Error_Handler();
+    }
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+    /** Initializes the CPU, AHB and APB buses clocks
+     */
+    RCC_ClkInitStruct.ClockType      = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider  = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK) {
+        Error_Handler();
+    }
 }
 
 /* USER CODE BEGIN 4 */
@@ -275,44 +290,45 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
-  if(htim->Instance == TIM1)
-  {
-    //send next bit
-    word_to_send[0] % 2 == 0 ? SET_BIT(word_to_send[0], 0) : CLEAR_BIT(word_to_send[0], 0);
-
-  }
+    if (htim->Instance == TIM1) {
+        // send next bit
+        word_to_send[0] % 2 == 0 ? SET_BIT(word_to_send[0], 0) : CLEAR_BIT(word_to_send[0], 0);
+    }
 }
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
-  if(huart->Instance == LPUART1)
-  {
-    if(freq_flag == 79) // O is for over (frequency is equal to 110kHz)
-    {
-      freq_flag = 0;  
-      PERIOD = 10900;
-      HRTIM1->sTimerxRegs[HRTIM_TIMERINDEX_TIMER_A].PERxR = PERIOD; //;
-      HAL_UART_Transmit(&hlpuart1, (uint8_t*)"Overdrive\n", 10, 100);
-    }
+    if (huart->Instance == LPUART1) {
+        if (freq_flag == 79) // O is for over (frequency is equal to 110kHz)
+        {
+            freq_flag                                            = 0;
+            PERIOD                                               = OVER;
+            HRTIM1->sTimerxRegs[HRTIM_TIMERINDEX_TIMER_A].PERxR  = PERIOD; //;
+            HRTIM1->sTimerxRegs[HRTIM_TIMERINDEX_TIMER_A].CMP1xR = PERIOD / 2;
+            HRTIM1->sTimerxRegs[HRTIM_TIMERINDEX_TIMER_A].CMP2xR = 0;
+            HRTIM1->sTimerxRegs[HRTIM_TIMERINDEX_TIMER_A].CMP3xR = 0;
+            HAL_UART_Transmit(&hlpuart1, (uint8_t *)"Overdrive\n", 10, 100);
+        }
 
-    else if(freq_flag == 66) // B is for base (frequency is equal to 85kHz)
-    {
-      freq_flag = 0;
-      PERIOD = 15500;
-      HRTIM1->sTimerxRegs[HRTIM_TIMERINDEX_TIMER_A].PERxR = PERIOD; //;
-      HAL_UART_Transmit(&hlpuart1, (uint8_t*)"Base\n", 4, 100);
+        else if (freq_flag == 66) // B is for base (frequency is equal to 85kHz)
+        {
+            freq_flag                                            = 0;
+            PERIOD                                               = BASE;
+            HRTIM1->sTimerxRegs[HRTIM_TIMERINDEX_TIMER_A].PERxR  = PERIOD; //;
+            HRTIM1->sTimerxRegs[HRTIM_TIMERINDEX_TIMER_A].CMP1xR = PERIOD / 2;
+            HRTIM1->sTimerxRegs[HRTIM_TIMERINDEX_TIMER_A].CMP2xR = 0;
+            HRTIM1->sTimerxRegs[HRTIM_TIMERINDEX_TIMER_A].CMP3xR = 0;
+            HAL_UART_Transmit(&hlpuart1, (uint8_t *)"Base\n", 4, 100);
+        }
     }
-    
-  }
-  HAL_UART_Receive_IT(&hlpuart1, (uint8_t*)&freq_flag, 1);
+    HAL_UART_Receive_IT(&hlpuart1, (uint8_t *)&freq_flag, 1);
 }
 
 int dec2bin(int num)
 {
     int bin = 0, k = 1;
 
-    while (num)
-    {
+    while (num) {
         bin += (num % 2) * k;
         k *= 10;
         num /= 2;
@@ -323,32 +339,32 @@ int dec2bin(int num)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
+    /* USER CODE BEGIN Error_Handler_Debug */
     /* User can add his own implementation to report the HAL error return state */
     __disable_irq();
     while (1) {
     }
-  /* USER CODE END Error_Handler_Debug */
+    /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
+    /* USER CODE BEGIN 6 */
     /* User can add his own implementation to report the file name and line number,
        ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+    /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
